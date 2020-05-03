8c717ea2b39a6b528b07ced01ed14c629fce8129
Digil Kumar <Digil Kumar@DESKTOP-NTE5G97>
2020-05-03 15:36:47 +0530

    updated commit

---
+++ NotesManager/src/com/notes/entity/ResponseCreateUser.java
@@ -0,0 +1,15 @@
+package com.notes.entity;
+
+public class ResponseCreateUser {
+private String userStatus;
+
+public String getUserStatus() {
+	return userStatus;
+}
+
+public void setUserStatus(String userStatus) {
+	this.userStatus = userStatus;
+}
+
+
+}
